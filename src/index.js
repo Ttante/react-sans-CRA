0cbfe02ee54b32173877d507a9bed5599e673f0a
Tyler Dinner <tylerdinner@gmail.com>
2020-09-25 16:44:31 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+const root = document.getElementById('root');
+
+
+const App = () => {
+  return (
+    <h1>Testerson</h1>
+  );
+}
+
+ReactDom.render(<App />, root)
